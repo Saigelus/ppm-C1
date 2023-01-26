685127e69b42f494f493ca1ed94be83918777f2d
Saigelus <116929138+Saigelus@users.noreply.github.com>
2023-01-25 20:49:43 -0400

    Add files via upload

---
+++ Ejercicios resueltos/Ejercicios hechos/Capítulo 1/Programa 1.9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*Estancia
+ El programa, al recibir como dato la superficie de una estancia expresada 
+ en acres, la convierte a hectáreas.
+ 
+ ECA: variable de tipo real.  */
+
+ void main(void)
+ {
+  float ECA;
+  printf("Ingrese la extensión de la estancia: ");
+  scanf("%f", &ECA);
+  ECA = ECA * 4047 /10000;
+  printf("\nExtensión de la estancia en hectáreas: %5.2f", ECA);
+ }
