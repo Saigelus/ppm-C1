005d77b7b74d717a1f7feeb737fd4d72b5c53922
Saigelus <116929138+Saigelus@users.noreply.github.com>
2023-04-21 06:50:49 -0400

    Add files via upload

---
+++ Programas en Cs/Programa01_03/Form1.cs
@@ -0,0 +1,57 @@
+namespace Programa01_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResultado.Text = "";
+            txtA.Text = "0";
+            txtB.Text = "0";
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a + b;
+
+            lblResultado.Text = r.ToString();
+        }
+
+        private void lblResta_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a - b;
+
+            lblResultado.Text = r.ToString();
+        }
+
+        private void lblMultiplicación_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a * b;
+
+            lblResultado.Text = r.ToString();
+        }
+
+        private void lblDivisión_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a / b;
+
+            lblResultado.Text = r.ToString();
+        }
+    }
+}
