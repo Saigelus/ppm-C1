ce08291f32a3ee97f447593bcc36d00a89da2570
Saigelus <116929138+Saigelus@users.noreply.github.com>
2023-04-21 06:51:40 -0400

    Add files via upload

---
+++ Programas en Cs/Programa02_01/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Programa02_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMostrar = new Button();
+            lblMensaje = new Label();
+            SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.Location = new Point(328, 63);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(94, 29);
+            btnMostrar.TabIndex = 0;
+            btnMostrar.Text = "Mostrar";
+            btnMostrar.UseVisualStyleBackColor = true;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // lblMensaje
+            // 
+            lblMensaje.AutoSize = true;
+            lblMensaje.Location = new Point(57, 67);
+            lblMensaje.Name = "lblMensaje";
+            lblMensaje.Size = new Size(50, 20);
+            lblMensaje.TabIndex = 1;
+            lblMensaje.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(485, 361);
+            Controls.Add(lblMensaje);
+            Controls.Add(btnMostrar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnMostrar;
+        private Label lblMensaje;
+    }
+}
