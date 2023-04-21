2dcdc635ac35f6ca16aa69b27d278236c42c9228
Saigelus <116929138+Saigelus@users.noreply.github.com>
2023-04-21 06:52:16 -0400

    Add files via upload

---
+++ Programas en Cs/Programa02_02/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Programa02_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAbrir = new Button();
+            txtMensaje = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            btnAbrir.Location = new Point(253, 46);
+            btnAbrir.Name = "btnAbrir";
+            btnAbrir.Size = new Size(94, 29);
+            btnAbrir.TabIndex = 0;
+            btnAbrir.Text = "Abrir";
+            btnAbrir.UseVisualStyleBackColor = true;
+            btnAbrir.Click += btnAbrir_Click;
+            // 
+            // txtMensaje
+            // 
+            txtMensaje.Location = new Point(32, 48);
+            txtMensaje.Name = "txtMensaje";
+            txtMensaje.Size = new Size(125, 27);
+            txtMensaje.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(389, 325);
+            Controls.Add(txtMensaje);
+            Controls.Add(btnAbrir);
+            Name = "Form1";
+            Text = "Forma 1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAbrir;
+        private TextBox txtMensaje;
+    }
+}
